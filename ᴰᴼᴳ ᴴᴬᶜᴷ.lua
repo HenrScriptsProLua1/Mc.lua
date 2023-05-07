7b275a434cb22762e6c77fd95392e3ac2720da13
HenrScriptsProLua1 <132516240+HenrScriptsProLua1@users.noreply.github.com>
2023-05-06 20:30:46 -0600

    Create ᴰᴼᴳ ᴴᴬᶜᴷ.lua

---
+++ ᴰᴼᴳ ᴴᴬᶜᴷ.lua
@@ -0,0 +1,233 @@
+gg.alert("\n  ║▌│█║▌│█║▌│█│║▌║▌│█║▌│█\n\n  🆂🅲🆁🅸🅿🆃 🅱🆈 🅷🅴🅽🆁🆈 🆂 🐰🍫☠️ₜₑₛₜ ᵥᵢₚ ₚₑₐCₕY ₑᵥₑₙₜ ☠️🍫🐰ㅤㅤㅤㅤㅤ ㅤ🇼​🇪​🇱​🇨​🇴​🇲​🇪​ 🇹​🇴​ 🇭​🇪​🇳​🇷​🇾​ 🇸​ 🇲​🇴​🇩​ 🇸​🇨​🇷​🇮​🇵​🇹​ 🇭​🇦​🇨​🇰​ ══┈┈┈┄┄╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┄┄┈┈┈┈┄┈┈┈┈┈┄┄╌╌╌╌══\n⊚ 🐰🍫☠️ʀᴇᴀᴅʏ-ᴛᴏ-ᴜꜱᴇ ꜱᴄʀɪᴘᴛ ꜱᴇʟᴇᴄᴛꜱ ᴄᴏɴᴠᴇɴɪᴇɴᴛ ᴏᴘᴛɪᴏɴꜱ ☠️🍫🐰Nᴇᴡ Sᴄʀɪᴘᴛ ᴠ1 1.69.x \n⊚ ☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️\n══┈┈┈┄┄╌╌╌╌┄┄┈╌╌╌╌╌╌╌╌╌╌╌╌╌╌┈┈┈┄┈┈┈┈┈┄┄╌╌╌╌══")
+gg.toast("ꈜꏳꌅ꒐ꉣꋖ ꏳꌅꁲꑒꐔ ꋖꌈꃔ꒐ꈜ꒐ꁲꃔꁲ ꅐꑀ꒒ꏳꊿꂵ ꋖꊿ ꃳꑀꐔ ꂵꐔ ꁝꁲꏳꈵ ꃳꐔ ꌅꑀꏳꊿꃔ")
+gg.toast ("WAITE TO CONECTED ᔕHenry S Op,")
+---LOADING STEP-[2] (💥Henry S Pro op hack💥)---
+
+
+function HOME()
+SN = gg.choice({
+   "╭───────────────────────────────────|\n📂DₒG ₕₐCₖ☞ₘₐₑₗₛₜᵣₒₘ(ᴸᵒᵇᵇʸ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️ㅤㅤㅤㅤ🇭​\n╰───────────────────────────────────|",
+   "╭───────────────────────────────────|\n📂DₒG ₕₐCₖ ᵢₙ ₘₐₑₗₛₜᵣₒₘ(ᴵⁿᵍᵃᵐᵉ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️ㅤㅤㅤ🇪​\n╰───────────────────────────────────|",
+   "╭───────────────────────────────────|\n📂FᵤₙCₜᵢₒₙ FᵣₑₑZₑ ₚᵢₛₜₒₗ(ᴸᵒᵇᵇʸ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️ㅤㅤㅤㅤ🇳​\n╰───────────────────────────────────|",
+   "╭───────────────────────────────────|\n📂ₘₛg₃₃↬FᵣₑₑZₑ ₚᵢₛₜₒₗ(ᴵⁿ ᵍᵃᵐᵉ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️ㅤㅤㅤㅤ🇷​\n╰───────────────────────────────────|",
+   "╭───────────────────────────────────|\n📂🅰🅸🅼🅱🅾🆃 ☞ (ᴵⁿ ᵍᵃᵐᵉ)☠️ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ☠️ㅤㅤㅤㅤ🇾​\n╰───────────────────────────────────|",
+   "ㅤㅤㅤㅤㅤㅤ╭───────────────────────────╮\n ㅤㅤㅤㅤㅤㅤㅤ☠️━─━──── E⃢​    X⃢​    I⃢​    T⃢─────━─━☠️\nㅤㅤㅤㅤㅤㅤ╰───────────────────────────╯",
+	 }, nil, "🇭​🇪​🇳​🇷​🇾​ 🇸​ 🇲​🇴​🇩​ 🇸​🇨​🇷​🇮​🇵​🇹​ 🇭​🇦​🇨​🇰​ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ☠️🅴🆂🅿🅴🅲🅸🅰🅻 🆂🅲🆁🅸🅿🆃 ㅤㅤㅤDₒG ₕₐCₖㅤㅤㅤ 🅽🅴🆆 🅼🅴🅽🆄 1.6.7 🆅17 🅱🆈 🅷🅴🅽🆁🆈 🆂 🅾🅿☠️.")
+if SN == nil then else
+if SN==1 then  HENRY1() end
+if SN==2 then HENRY2() end
+if SN==3then HENRY3() end
+if SN==4 then HENRY4() end
+if SN==5 then HENRY5() end
+if SN==6 then Exit() end
+end 
+
+
+  PUBGMH = -1
+end
+
+function HENRY1()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"51.0F;233377.0F;233568.0F:41"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('"51"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll('"1.0e10"', gg.TYPE_FLOAT)
+
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = '"1.0e10"'
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("35.0F;370370.0F;370371.0F:245", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("35", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.00000003e16", gg.TYPE_FLOAT)
+
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "1.00000003e16"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("57.0F;49.0F;50.0F;59.0F;36.5F;35.0F:101", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("59", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(50, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("-185", gg.TYPE_FLOAT)
+
+revert = gg.getResults(50, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(50, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "-185.0"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('Hack DₒG ₕₐCₖ ₛₙᵢₚₑᵣ=ON')
+end
+
+function HENRY2()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("260845;66;-1596977136:9", gg.TYPE_DWORD)
+gg.refineNumber("-1596977136", gg.TYPE_DWORD)
+revert = gg.getResults(1)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "-1996488704"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast("DₒG ₕₐCₖ CODE FREEZE 🥶")
+end
+
+
+function HENRY3()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("59.0;134.0;1033.0:73", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("59", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_FLOAT then
+v.value = "-172"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("10034.0;130.0;34.34999847412:9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("34.34999847412", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("35.7", gg.TYPE_FLOAT)
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_FLOAT then
+v.value = "35.5"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('🥶FREEZER = ON🥶')
+end
+
+
+
+
+function HENRY4()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("-2.64835626e-23;-1.0;40.0:65", gg.TYPE_FLOAT)
+gg.refineNumber("-2.64835626e-23", gg.TYPE_FLOAT)
+revert = gg.getResults(1)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+if v.flags == gg.TYPE_FLOAT then
+v.value = "-5.39260972e-33"
+v.freeze = true
+end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.toast('🥶FREEZER = ON🥶 OPTION ACTIVATION IN GAME')
+end
+
+
+function HENRY5()
+gg.searchNumber("0.005;0.180", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("9999.8", gg.TYPE_FLOAT)
+if revert ~= nil then gg.setValues(revert) end
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("9999.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("🅰🅸🅼🅱🅾🆃")
+
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-0.0001", gg.TYPE_FLOAT)
+gg.getResults(10)
+gg.editAll("9999.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("👀🅰🅸🅼🅱🅾🆃 AND ᵢₘₚₛ ₛₙᵢₚₑᵣ👀")
+end
+
+
+
+
+function Exit()
+gg.toast('MENU HACK MODERN COMBAT 5 by ђєภภy ร ๏P ђคcк')
+print("      ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀  ")
+    print("══════════════⋆✪⋆══════════════╢")
+    print(": ⠄⠄⠄⠄ ⠄⠄⠄⠄ ⠄⠄⠄⠄")
+    print("⠄⠄⡔⠙⠢⡀⠄⠄⠄⢀⠼⠅⠈⢂⠄⠄⠄⠄")
+    print("⠄⠄⡌⠄⢰⠉⢙⢗⣲⡖⡋⢐⡺⡄⠈⢆⠄⠄⠄")
+    print("⠄⡜⠄⢀⠆⢠⣿⣿⣿⣿⢡⢣⢿⡱⡀⠈⠆⠄⠄")
+    print("⠄⠧⠤⠂⠄⣼⢧⢻⣿⣿⣞⢸⣮⠳⣕⢤⡆⠄⠄")
+    print("⢺⣿⣿⣶⣦⡇⡌⣰⣍⠚⢿⠄⢩⣧⠉⢷⡇⠄⠄")
+    print("⠘⣿⣿⣯⡙⣧⢎⢨⣶⣶⣶⣶⢸⣼⡻⡎⡇⠄⠄")
+    print("⠄⠘⣿⣿⣷⡀⠎⡮⡙⠶⠟⣫⣶⠛⠧⠁⠄⠄⠄")
+    print(" ⠄⠄⠘⣿⣿⣿⣦⣤⡀⢿⣿⣿⣿⣄⠄⠄⠄⠄⠄")
+    print("⠄⠄⠄⠈⢿⣿⣿⣿⣿⣷⣯⣿⣿⣷⣾⣿⣷⡄⠄")
+    print("⠄⠄⠄⠄⠄⢻⠏⣼⣿⣿⣿⣿⡿⣿⣿⣏⢾⠇⠄")
+    print("⠄⠄⠄⠄⠄⠈⡼⠿⠿⢿⣿⣦⡝⣿⣿⣿⠷⢀⠄")
+    print("⠄⠄⠄⠄⠄⠄⡇⠄⠄⠄⠈⠻⠇⠿⠋⠄⠄⢘⡆")
+    print("⠄⠄⠄⠄⠄⠄⠱⣀⠄⠄⠄⣀⢼⡀⠄⢀⣀⡜⠄")
+    print("⠄⠄⠄⠄⠄⠄⠄⢸⣉⠉⠉⠄⢀⠈⠉⢏⠁⠄⠄")
+    print("⠄⠄⠄⠄⠄⠄⡰⠃⠄⠄⠄⠄⢸⠄⠄⢸⣧⠄⠄")
+    print("⠄⠄⠄⠄⠄⣼⣧⠄⠄⠄⠄⠄⣼⠄⠄⡘⣿⡆⠄")
+    print("⠄⠄⠄⢀⣼⣿⡙⣷⡄⠄⠄⠄⠃⠄⢠⣿⢸⣿⡀")
+    print("⠄⠄⢀⣾⣿⣿⣷⣝⠿⡀⠄⠄⠄⢀⡞⢍⣼⣿⠇")
+    print("⠄⠄⣼⣿⣿⣿⣿⣿⣷⣄⠄⠄⠠⡊⠴⠋⠹⡜⠄")
+    print("⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⡆⣤⣾⣿⣿⣧⠹⠄⠄")
+    print("⠄⠄⢿⣿⣿⣿⣿⣿⣿⣿⢃⣿⣿⣿⣿⣿⡇⠄⠄")
+    print("⠄⠄⠐⡏⠉⠉⠉⠉⠉⠄⢸⠛⠿⣿⣿⡟⠄")
+    print("⠄⠄⠐★ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ★⠄")
+    
+    
+--★ʜᴇɴʀʏ ꜱ ꜱᴄʀɪᴘᴛ★
+    print("╟══════════════⋆✪⋆══════════════╢")
+    
+    print("      ▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀  \n \n")
+os.exit()
+main()
+end
+while true do
+  if gg.isVisible(true) then
+    PUBGMH = 1
+    gg.setVisible(false)
+  end
+  gg.clearResults()
+  if PUBGMH == 1 then
+    HOME()
+ end
+ end
